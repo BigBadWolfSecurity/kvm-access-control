fd007f31cdd1258b6075f883fd623bed33051333
Adam Pryor <adam@bigbadwolfsecurity.com>
2022-07-28 15:18:32 -0500

    can ping vm to vm

--- virtual-networking.sh
+++ virtual-networking.sh
@@ -5,24 +5,27 @@ modprobe br_netfilter
 
 # https://wiki.archlinux.org/title/network_bridge
 
-ip link del testbr0
-ip link del fedora
-ip link del testb
+ip link del left
+ip link del right
+ip netns delete namespace1
 
 function create_bridge() {
-    ip link add name $1 type bridge
-    ip link set $1 up
-    ip addr add $2 dev $1
+    ip netns exec $4 ip link add name $1 type bridge
+    ip netns exec $4 ip link set $1 up
+    ip netns exec $4 ip addr add $2 dev $1
+    ip netns exec $4 ip addr add $3 dev $1
 }
 
 function create_veth() {
     ip link add $1 type veth peer name $2
-    ip link set $2 master $3
-    ip link set $2 up
+    ip link set $2 netns $4
+    ip netns exec $4 ip link set $2 master $3
+    ip netns exec $4 ip link set $2 up
 }
 
-create_bridge testbr0 192.168.5.3/24
-create_veth fedora fedora2testbr0 testbr0
-create_veth testb testb2testbr0 testbr0
+ip netns add namespace1
+create_bridge testbr0 192.168.5.3/24 192.168.6.3/24 namespace1
+create_veth left lefttestbr0 testbr0 namespace1
+create_veth right righttestbr0 testbr0 namespace1
 
-ip link set testbr0 up
+ip netns exec namespace1 ip link set testbr0 up
