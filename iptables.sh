a5b9eff1f477d469c84ee83c178433d761631ae3
Adam Pryor <adam@bigbadwolfsecurity.com>
2022-07-29 11:39:18 -0500

    adam gives up

--- iptables.sh
+++ iptables.sh
@@ -25,6 +25,28 @@ ip netns exec namespace1 iptables -t mangle -A PREROUTING -s 192.168.6.0/24 -j S
 iptables -t mangle -A FORWARD -s 192.168.6.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
 ip netns exec namespace1 iptables -t mangle -A FORWARD -s 192.168.6.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
 
+iptables -t mangle -A INPUT -i left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A INPUT -i lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A OUTPUT -o left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A OUTPUT -o lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A POSTROUTING -o left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A POSTROUTING -o lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A PREROUTING -i left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A PREROUTING -i lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A FORWARD -i left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A FORWARD -i lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+
+iptables -t mangle -A INPUT -i right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A INPUT -i righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A OUTPUT -o right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A OUTPUT -o righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A POSTROUTING -o right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A POSTROUTING -o righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A PREROUTING -i right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A PREROUTING -i righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t mangle -A FORWARD -i right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t mangle -A FORWARD -i righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+
 # Security table labelling
 iptables -t security -A INPUT -s 192.168.5.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
 ip netns exec namespace1 iptables -t security -A INPUT -s 192.168.5.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
@@ -39,6 +61,20 @@ iptables -t security -A OUTPUT -s 192.168.6.0/24 -j SECMARK --selctx system_u:ob
 ip netns exec namespace1 iptables -t security -A OUTPUT -s 192.168.6.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
 iptables -t security -A FORWARD -s 192.168.6.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
 ip netns exec namespace1 iptables -t security -A FORWARD -s 192.168.6.0/24 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+
+iptables -t security -A INPUT -i left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t security -A INPUT -i lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t security -A OUTPUT -o left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t security -A OUTPUT -o lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t security -A FORWARD -i left -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t security -A FORWARD -i lefttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+
+iptables -t security -A INPUT -i right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t security -A INPUT -i righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t security -A OUTPUT -o right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t security -A OUTPUT -o righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+iptables -t security -A FORWARD -i right -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
+ip netns exec namespace1 iptables -t security -A FORWARD -i righttestbr0 -j SECMARK --selctx system_u:object_r:bare_packet_t:s0
 
 iptables -I FORWARD -i left -o right -s 192.168.5.0/24 -d 192.168.6.0/24 -j ACCEPT
 ip netns exec namespace1 iptables -I FORWARD -i left -o right -s 192.168.5.0/24 -d 192.168.6.0/24 -j ACCEPT
