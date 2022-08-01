21ba9cf9f83f863784ee7e4dd50252d95bca721b
Adam Pryor <adam@bigbadwolfsecurity.com>
2022-08-01 16:32:48 -0500

    updates

--- cipso.sh
+++ cipso.sh
@@ -4,11 +4,14 @@
 
 netlabelctl cipsov4 del doi:1
 #netlabelctl cipsov4 del doi:2
-#netlabelctl cipsov4 add pass doi:1 tags:1
-netlabelctl cipsov4 add local doi:1
+netlabelctl cipsov4 add pass doi:1 tags:1
+#netlabelctl cipsov4 add local doi:1
 
-netlabelctl map del default
-netlabelctl map add default address:0.0.0.0/0 protocol:unlbl  
-netlabelctl map add default address:::/0 protocol:unlbl
-netlabelctl map add default address:127.0.0.1 protocol:cipsov4,1
-netlabelctl map add default address:192.168.5.3 protocol:cipsov4,1
+#netlabelctl map del default
+#netlabelctl map add default address:0.0.0.0/0 protocol:unlbl  
+#netlabelctl map add default address:::/0 protocol:unlbl
+
+netlabelctl unlbl add interface:macvtap0 address:0.0.0.0/0 label:system_u:object_r:left_vm_t:s0
+netlabelctl unlbl add interface:macvtap1 address:0.0.0.0/0 label:system_u:object_r:right_vm_t:s0
+netlabelctl unlbl add interface:left address:0.0.0.0/0 label:system_u:object_r:left_vm_t:s0
+netlabelctl unlbl add interface:right address:0.0.0.0/0 label:system_u:object_r:right_vm_t:s0
